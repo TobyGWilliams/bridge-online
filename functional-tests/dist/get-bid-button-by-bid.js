c8d87ad2241421be2269b647c61c0199e4fcfaa3
Toby Williams <Toby.Williams@waitrose.co.uk>
2020-11-15 17:24:04 +0000

    remove compiled code

--- functional-tests/dist/get-bid-button-by-bid.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (bid) {
-    return "button[data-test=\"place-bid\"][data-test-bid=\"" + JSON.stringify(bid).replace(/\"/g, '\\"') + "\"]";
-});
--- functional-tests/dist/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var messageCount = 0;
-exports.default = (function (message) {
-    messageCount += 1;
-    console.log("\t" + (messageCount < 10 ? " " : "") + messageCount + ". " + message);
-});
